1e77516787ee4a2823ae0cbfa7f375801362fa6b
dogan-keles <131661707+dogan-keles@users.noreply.github.com>
2023-10-22 15:53:52 +0300

    Spring Boot Dependency Injection Project

---
+++ src/main/java/com/workintech/dependencyproject/model/MidDeveloper.java
@@ -0,0 +1,9 @@
+package com.workintech.dependencyproject.model;
+
+public class MidDeveloper extends Developer{
+    public MidDeveloper(int id, String name, double salary) {
+        super(id, name, salary, Experience.MID);
+    }
+
+
+}
